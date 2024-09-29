0088cd9e535b6e13cc04a32929a1221865241d60
Manmi13 <xplus1@proton.me>
2024-09-29 18:19:08 +0200

    Update parser_part1.py

--- parser_part1.py
+++ parser_part1.py
@@ -9,59 +9,43 @@ from bs4 import BeautifulSoup
 import time
 import sqlite3
 
-# Настраиваем драйвер
 chrome_driver_path = r'C:\chromedriver-win64\chromedriver.exe'
 chrome_options = Options()
-chrome_options.add_argument('--headless')  # Запустим в фоновом режиме
+chrome_options.add_argument('--headless') 
 chrome_options.add_argument('window-size=640,480')
-chrome_options.add_argument('--disable-extensions')  # Отключает расширения
+chrome_options.add_argument('--disable-extensions') 
 chrome_options.add_argument('--disable-infobars')
 
 service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# URL страницы входа и страницы с данными
-login_url = r'https://admin.biletyna.pl/auth'
-data_url = r'https://admin.biletyna.pl/instructor-app/course/view/60456'
+login_url = r'https://admin.site.pl/auth'
+data_url = r'https://admin.site.pl/app/course/view/6070'
 
-# Открываем страницу входа
 driver.get(login_url)
 
-# login =
-# password =
+driver.find_element(By.ID, 'login').send_keys('mymail@gmail.com')
+driver.find_element(By.ID, 'pass').send_keys('mypass')
 
-# Заполняем форму
-driver.find_element(By.ID, 'login').send_keys('mirsantaki.20@gmail.com')
-driver.find_element(By.ID, 'pass').send_keys('rock24Ka')
-
-# Отправляем форму
 driver.find_element(By.ID, 'pass').send_keys(Keys.RETURN)
 
-# Явное ожидание загрузки следующей страницы
 WebDriverWait(driver, 10).until(EC.url_changes(login_url))
 
-# Открываем страницу с данными
 driver.get(data_url)
 
-# Явное ожидание загрузки таблицы
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, 'customer_record')))
 
-# Получаем HTML содержимое
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-# Находим таблицу
 table = soup.find('table', class_='table table-responsive table-hover')
 
-# Находим все строки таблицы
 rows = table.find_all('tr', class_='customer_record')
 
-# Список для хранения данных
 records = []
 
-# Обрабатываем каждую строку
 for row in rows:
-    cols = row.find_all('td')  # Находим все ячейки в строке
+    cols = row.find_all('td')
     record = {
         'User': cols[0].text.strip(),
         # 'Grupa': cols[1].text.strip(),
@@ -75,10 +59,8 @@ for row in rows:
 
 driver.quit()
 
-connection = sqlite3.connect(r'C:\Users\user\Desktop\SQLite\contacts.db')
+connection = sqlite3.connect(r'C:\MyProject\contacts.db')
 cursor = connection.cursor()
-
-# debt_list = []
 
 for i in records:
     cursor.execute('''
@@ -96,7 +78,7 @@ else:
 
 connection.close()
 
-with open(r'C:\Users\user\Desktop\python\debts.txt', 'a') as f:
+with open(r'C:\MyProject\debts.txt', 'a') as f:
     f.write('\n' + time.ctime() + '\n' + '\n')
 
     for i in records:
